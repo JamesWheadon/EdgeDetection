dda78b69ea73751a446f8d5b1840b54d8f6fd756
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 12:25:07 +0100

    vertical filter for edges

--- API/controllers/edges.py
+++ API/controllers/edges.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
 
+
+horizontal_sobel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
 def welcome(req):
     return "Waiting for an image", 200
 
@@ -8,6 +11,19 @@ def image(req):
     image = req.files.to_dict()['image']
     img = Image.open(image).convert('L')
     img.show()
-    pixels = np.array(img)
-    print(pixels)
+    vertical_image = Image.fromarray(np.uint8(vertical_edges(img)))
+    vertical_image.show()
     return "Recieved image", 201
+
+def vertical_edges(image):
+    pixels = np.array(image)
+    vertical_sobel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    width, height = image.size
+    new_image = np.zeros((width, height))
+    for i in range(1, width-1):
+        for j in range(1, height-1):
+            pixel_kernel = pixels[i-1 : i+2, j-1 : j+2]
+            print(pixel_kernel)
+            value = np.sum(pixel_kernel * vertical_sobel)
+            new_image[i, j] = value
+    return new_image
