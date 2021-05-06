a592c38af9f8e3bceeb36f3d93311114348a58cb
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 10:13:44 +0100

    add componenet to upload an image

---
+++ Client/src/components/ImageUpload/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ImageUpload() {
+
+    return (
+        <form role="form">
+            <label>
+                <input type="file"/>
+            </label>
+        </form>
+    );
+};
+
+export default ImageUpload;
--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ImageUpload } from '../../components';
 
 const Home = () => {
  
     return (
         <>
             <h1>Hello World!</h1>
+            <ImageUpload />
         </>
     )
 }
