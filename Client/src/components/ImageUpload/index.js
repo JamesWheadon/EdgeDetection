ac5101802d714316369d60d72bf335363c42208a
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 10:45:36 +0100

    upload file

--- Client/src/components/ImageUpload/index.js
+++ Client/src/components/ImageUpload/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function ImageUpload() {
 
+    const [image, setImage] = useState("")
+
+    const updateImage = e => {
+        console.log(e)
+        const input = URL.createObjectURL(e.target.files[0])
+        console.log(input)
+        setImage(input)
+        console.log(image)
+    }
+
     return (
+        <>
         <form role="form">
             <label>
-                <input type="file"/>
+                <input type="file" onChange={updateImage} />
             </label>
         </form>
+        <img src={image}/>
+        </>
     );
 };
 
