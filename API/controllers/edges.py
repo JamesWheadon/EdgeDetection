0897685cfb6ef80d764b0943eb2b2f2529a4b1b2
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 12:12:23 +0100

    numpy in api to get matrix of pixel values

--- API/controllers/edges.py
+++ API/controllers/edges.py
@@ -1,10 +1,13 @@
 from PIL import Image
+import numpy as np
 
 def welcome(req):
     return "Waiting for an image", 200
 
 def image(req):
     image = req.files.to_dict()['image']
-    img = Image.open(image).convert('LA')
+    img = Image.open(image).convert('L')
     img.show()
+    pixels = np.array(img)
+    print(pixels)
     return "Recieved image", 201
