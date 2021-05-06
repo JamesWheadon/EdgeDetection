255ae1c2f73ab4e54c40cdcb9b2e06f2fce627a3
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 13:02:50 +0100

    apply both filters and save image in api

--- API/controllers/edges.py
+++ API/controllers/edges.py
@@ -1,5 +1,6 @@
-from PIL import Image
+import math
 import numpy as np
+from PIL import Image
 
 def welcome(req):
     return "Waiting for an image", 200
@@ -7,11 +8,14 @@ def welcome(req):
 def image(req):
     image = req.files.to_dict()['image']
     img = Image.open(image).convert('L')
-    img.show()
-    vertical_image = Image.fromarray(np.uint8(vertical_edges(img)))
-    vertical_image.show()
-    horizontal_image = Image.fromarray(np.uint8(horizontal_edges(img)))
-    horizontal_image.show()
+    v_edges = vertical_edges(img)
+    h_edges = horizontal_edges(img)
+    both_edges = np.sqrt((np.square(v_edges) + np.square(h_edges)))
+    both_edges = np.clip(both_edges.astype(int), 0, 255)
+    both_edges = np.flip(np.rot90(both_edges), 0)
+    edges_image = Image.fromarray(np.uint8(both_edges))
+    edges_image.show()
+    edges_image.save('filteredImage.jpg')
     return "Recieved image", 201
 
 def vertical_edges(image):
@@ -28,8 +32,6 @@ def vertical_edges(image):
             value = np.sum(pixel_kernel * vertical_sobel)
             new_image[i, j] = value
     new_image = np.clip(new_image, 0, 255)
-    new_image = np.rot90(new_image)
-    new_image = np.flip(new_image, 0)
     return new_image
 
 def horizontal_edges(image):
@@ -46,6 +48,4 @@ def horizontal_edges(image):
             value = np.sum(pixel_kernel * horizontal_sobel)
             new_image[i, j] = value
     new_image = np.clip(new_image, 0, 255)
-    new_image = np.rot90(new_image)
-    new_image = np.flip(new_image, 0)
     return new_image
