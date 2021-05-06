600c7521e5a0eb6d1e9f6368c453e86d1fa534d8
James Wheadon <james.wheadon@yahoo.com>
2021-05-06 09:38:43 +0100

    flask route for edges

---
+++ API/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Waiting for images"}), 200
+
+@app.route('/edges', methods=['GET', 'POST'])
+def edges_handler():
+    fns = {
+        'GET': edges.welcome,
+    }
+    resp, code = fns[request.method](request)
+
+    return jsonify(resp), code
+
+if __name__ == "__main__":
+    app.run(debug=True)
