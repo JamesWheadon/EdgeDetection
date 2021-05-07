09350a0d5ba6c47df1d5c6bec8ff6e6fd57404e7
James Wheadon <james.wheadon@yahoo.com>
2021-05-07 16:06:12 +0100

    paint canvas with image data from api

--- Client/src/components/Canvas/index.js
+++ Client/src/components/Canvas/index.js
@@ -8,10 +8,21 @@ function Canvas({ pixels }) {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-        const id = context.getImageData(0, 0, canvasWidth, canvasHeight);
-        const canvasPixels = id.data;
-        print(canvasPixels)
-    }, [])
+        let id = context.createImageData(1,1); // only do this once per page
+        let d  = id.data;                        // only do this once per page
+        for (let row in pixels) {
+            for (let column in pixels[row]) {
+                d[0]   = pixels[row][column];
+                d[1]   = pixels[row][column];
+                d[2]   = pixels[row][column];
+                d[3]   = 255;
+                context.putImageData( id, column, row );
+            }
+        }
+        const test = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+        const canvasPixels = test.data;
+        console.log(canvasPixels)
+    }, [pixels])
 
     return <canvas height={pixels.length} width={pixels[0].length} ref={canvasRef} />
 };
